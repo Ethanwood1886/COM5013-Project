e135520e5ad783636ae9e98df253fae703589e46
Ethanwood1886 <22319448@bucks.ac.uk>
2024-12-06 14:13:52 +0000

    Read and Tokenize from a Word Doc

--- sorting.py
+++ sorting.py
@@ -1,13 +1,27 @@
-# Library for random words
+import re
+from docx import Document
 
-from faker import Faker
+def read_tokenize(file_path):
+    doc = Document(file_path)
+    text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
 
-fake = Faker()
+    words = re.findall(r'\b\w+\b', text)    # Extract words from document
 
-# Generate random words
+    words = [word.lower() for word in words]    # Make all words lowercase
+    
+    return words 
 
-small_list = [fake.word() for _ in range(100)]
-large_list = [fake.word() for _ in range(1000)]
+small_data = '100words.docx'
+large_data = '1000words.docx'
 
-print(small_list)
-print(large_list)
+token_small_data = read_tokenize(small_data)
+token_large_data = read_tokenize(large_data)
+
+print('Tokenised 100 Words:', token_small_data)
+print('Tokenised 1000 Words:', token_large_data)
+
+# Merge Sort
+
+
+    
+
