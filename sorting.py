24f3c8fcd9ba7f98d25f036d1bbdfe6a740bebdc
Ethanwood1886 <22319448@bucks.ac.uk>
2024-12-11 23:57:54 +0000

    Start of Merge Sort

--- sorting.py
+++ sorting.py
@@ -1,5 +1,6 @@
 import re
 from docx import Document
+import numpy as np
 
 def read_tokenize(file_path):
     doc = Document(file_path)
@@ -20,8 +21,21 @@ token_large_data = read_tokenize(large_data)
 print('Tokenised 100 Words:', token_small_data)
 print('Tokenised 1000 Words:', token_large_data)
 
-# Merge Sort
+def merge(left_right)
 
+# Merge Sort function to DIVIDE array into 2 halves
 
+def merge_sort(array):
+    if len(array) <= 1:              # if array have 1 or less elements, array is sorted
+        return array
+    
+    middle = len(array)//2           # divide the array into 2 halves
+    left_side = array[:middle]
+    right_side = array[middle:]
+    
+    left_side = merge_sort(left_side)
+    right_side = merge_sort(right_side)
+
+    return merge(left_side, right_side)
     
 
